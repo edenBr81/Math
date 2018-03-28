b4196a4de6721dab34c67b265779af50ac8fc2ca
Eden Bereda <edukenat@gmail.com>
2018-03-28 17:04:59 -0400

    Initial commit

---
+++ src/main/java/com/example/math/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.math;
+
+public class HomeController {
+
+    public static void main(String[] args) {
+        double mynumber , myothernumber;
+
+        mynumber = 2;
+        myothernumber= 1.7938;
+
+        System.out.println(" mynumber "  +  mynumber);
+        System.out.println(" myothernumber "  + myothernumber);
+    }
+}
